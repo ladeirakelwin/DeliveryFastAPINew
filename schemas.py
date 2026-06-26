de86c25f1818ced931147d0f7e190d37435efa28
ladeirakelwin <ladeirakelwin@gmail.com>
2026-06-26 19:57:18 -0300

    schemas inicias do usuário

---
+++ schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, ConfigDict
+from typing import Optional
+
+
+class UsuarioBaseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    nome: str
+    email: str
+    ativo: Optional[bool]
+    admin: Optional[bool]
+
+class UsuarioSchema(UsuarioBaseSchema):
+    model_config = ConfigDict(from_attributes=True)
+    
+    senha: str
+    
+
+class UsuarioResponseSchema(UsuarioBaseSchema):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
