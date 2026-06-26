fb9a5eb6a520431113f77fc1893dd80b32d5fbf2
ladeirakelwin <ladeirakelwin@gmail.com>
2026-06-26 19:59:03 -0300

    Rota de criação de usuário criada

--- routes/auth.py
+++ routes/auth.py
@@ -1,3 +1,15 @@
 from fastapi import APIRouter
+from fastapi.security import OAuth2PasswordBearer
+from schemas import UsuarioResponseSchema, UsuarioSchema
+from services.usuario_service import UsuarioService
+from database import DBSession
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 auth_routes = APIRouter(prefix="/auth", tags=["Auth Routes"])
+
+@auth_routes.post("/criar-conta", response_model=UsuarioResponseSchema)
+async def criar_conta(usuario: UsuarioSchema, db: DBSession ):
+    usuario_service = UsuarioService(db)
+    novo_usuario = usuario_service.criar_usuario(usuario.email, usuario.senha, usuario.nome, usuario.ativo, usuario.admin)
+
+    return UsuarioResponseSchema.model_validate(novo_usuario)
