7a7bf51fa5938b021f68728ea73c3e4a7bbacb54
ladeirakelwin <ladeirakelwin@gmail.com>
2026-06-27 20:22:17 -0300

    adaptando declarative base para o formato de classe

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dependencies import DATABASE_URL
 from fastapi import Depends
 from sqlalchemy.orm import Session
@@ -9,7 +9,8 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
-Base: DeclarativeBase = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 def get_db():
     db = SessionLocal()
