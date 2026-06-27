aad52cdcf2bc1e5d6483daca1ff47e546cbe14ce
ladeirakelwin <ladeirakelwin@gmail.com>
2026-06-26 21:14:05 -0300

    obtendo algoritmo de encriptação

--- dependencies.py
+++ dependencies.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY") 
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
