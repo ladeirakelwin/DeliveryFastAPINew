67803447f51808804b734694a6242c22fc622420
ladeirakelwin <ladeirakelwin@gmail.com>
2026-06-27 21:34:15 -0300

    removendo login schema

--- schemas.py
+++ schemas.py
@@ -19,10 +19,6 @@ class UsuarioResponseSchema(UsuarioBaseSchema):
     model_config = ConfigDict(from_attributes=True)
     id: int
 
-class LoginSchema(BaseModel):
-    username: str
-    password: str
-
 class TokenSchema(BaseModel):
     access_token: str
     token_type: str 
