c0fe9d4a0ee321e1c1c402a7e6aba3123298e527
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-01 21:54:09 -0300

    Adicionando refresh token expire secret

--- dependencies.py
+++ dependencies.py
@@ -8,5 +8,6 @@ DATABASE_URL = os.getenv("DATABASE_URL","")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = str(os.getenv("ALGORITHM"))
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 3))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login-form")
