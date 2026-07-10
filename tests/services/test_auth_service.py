df47d89353b720c866ba15bc9a26b7b075a418d9
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-09 22:12:27 -0300

    criando teste para validar se tokens criados em momentos diferentes tem expirações distintas

---
+++ tests/services/test_auth_service.py
@@ -0,0 +1,15 @@
+from services.auth_service import AuthService
+from time import sleep
+
+def test_se_tokens_criados_em_tempos_distintos_tem_expiracoes_diferentes():
+    auth_service: AuthService = AuthService()
+
+    token_1 = auth_service.criar_token({"sub": "teste2"})
+    sleep(1)
+    token_2 = auth_service.criar_token({"sub": "teste3"})
+
+    tempo_exp_1 = AuthService.decodificar_token(token_1)
+    tempo_exp_2 = AuthService.decodificar_token(token_2)
+
+    assert tempo_exp_1.get("exp") != tempo_exp_2.get("exp")
+    
