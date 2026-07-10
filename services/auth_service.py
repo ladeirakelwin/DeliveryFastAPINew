b2d4e874c4ab327601ec7f2b004071d4419ceb5e
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-09 22:15:04 -0300

    corrigindo tempo de expiração da criação do token

--- services/auth_service.py
+++ services/auth_service.py
@@ -7,7 +7,6 @@ from dependencies import (
 )
 from fastapi.exceptions import HTTPException
 from fastapi import status
-from datetime import timezone
 from jwt.exceptions import ExpiredSignatureError
 import jwt
 
@@ -18,24 +17,21 @@ class AuthService:
         detail="Erro ao gerar token! Tente novamente mais tarde.",
     )
 
-    def __init__(
-        self,
-        expiracao_token: int = ACCESS_TOKEN_EXPIRE_MINUTES,
-        refresh_expiracao_token: int = REFRESH_TOKEN_EXPIRE_DAYS,
-    ):
-        self.expiracao_token = datetime.now() + timedelta(minutes=expiracao_token)
-        self.refresh_expiracao_token = datetime.now() + timedelta(
-            days=refresh_expiracao_token
-        )
 
-    def criar_token(self, data: dict, is_refresh_token: bool = False) -> str:
+    def criar_token(self, data: dict, is_refresh_token: bool = False,expiracao_token: int = ACCESS_TOKEN_EXPIRE_MINUTES,
+        refresh_expiracao_token: int = REFRESH_TOKEN_EXPIRE_DAYS,) -> str:
         try:
+
+            tempo_expiracao_token = datetime.now(timezone.utc) + timedelta(minutes=expiracao_token)
+            tempo_refresh_expiracao_token = datetime.now(timezone.utc) + timedelta(
+                days=refresh_expiracao_token)
+            
             token_cru = data.copy()
             token_cru.update(
                 {
-                    "exp": self.expiracao_token
+                    "exp": tempo_expiracao_token
                     if not is_refresh_token
-                    else self.refresh_expiracao_token,
+                    else tempo_refresh_expiracao_token,
                     "type": "access" if not is_refresh_token else "refresh",
                 }
             )
