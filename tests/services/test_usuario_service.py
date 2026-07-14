c5650ae1574cb1af7b12901b5f08916b05381b4e
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-13 21:57:20 -0300

    testando login com usuários inválidos

--- tests/services/test_usuario_service.py
+++ tests/services/test_usuario_service.py
@@ -1,11 +1,13 @@
 from src.services.usuario_service import UsuarioService
-from models import Usuario
+from fastapi.exceptions import HTTPException
+import pytest
 
 VALID_USERS: list = [
     {"nome": "adm", "senha": "adm"},
     {"nome": "teste", "senha": "teste"},
 ]
 INVALID_USERS: list = [
+    {"nome": "nonecxiste", "senha": "nonecxiste"},
     {"nome": "inativoa", "senha": "inativoa"},
     {"nome": "inativoa", "senha": "inativoa"},
 ]
@@ -26,3 +28,32 @@ def test_usuario_service_se_consigo_autenticar_usuario_valido(user_seeded_db):
             and hasattr(usuario_autenticado, "admin")
             and hasattr(usuario_autenticado, "ativo")
         )
+
+
+def test_usuario_service_se_nao_consigo_logar_com_usuario_invalido_ou_inativo(
+    user_seeded_db,
+):
+    usuario_service = UsuarioService(user_seeded_db)
+
+    with pytest.raises(HTTPException) as scenario_1:
+        usuario_service.autenticar_usuario(
+            INVALID_USERS[0].get("nome"), INVALID_USERS[0].get("senha")
+        )
+        assert scenario_1.value.status_code == 401
+        assert scenario_1.value.detail == "Usuário ou senha inválido!"
+
+    with pytest.raises(HTTPException) as scenario_2:
+        usuario_service.autenticar_usuario(
+            INVALID_USERS[1].get("nome"), INVALID_USERS[1].get("senha")
+        )
+
+        assert scenario_2.value.status_code == 403
+        assert scenario_2.value.detail == "Usuário inativo!"
+
+    with pytest.raises(HTTPException) as scenario_3:
+        usuario_service.autenticar_usuario(
+            INVALID_USERS[2].get("nome"), INVALID_USERS[2].get("senha")
+        )
+
+        assert scenario_3.value.status_code == 403
+        assert scenario_3.value.detail == "Usuário inativo!"
