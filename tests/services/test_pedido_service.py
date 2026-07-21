a765627fc57c5a3ff0643bbb24f0437781f0683e
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-20 22:07:30 -0300

    testando se não é possível remover item do pedido com pedido inexistente

--- tests/services/test_pedido_service.py
+++ tests/services/test_pedido_service.py
@@ -167,16 +167,16 @@ def test_pedido_service_se_consigo_remover_item_de_um_pedido(order_item_seeded_d
 
 
 def test_pedido_service_se_consigo_remover_item_inexistente_de_um_pedido(
-    order_seeded_db,
+    order_item_without_order_seeded_db,
 ):
-    pedido_service = PedidoService(order_seeded_db)
-    usuario_service = UsuarioService(order_seeded_db)
+    pedido_service = PedidoService(order_item_without_order_seeded_db)
+    usuario_service = UsuarioService(order_item_without_order_seeded_db)
 
     usuario = usuario_service.autenticar_usuario(
         VALID_USERS[0].get("nome"), VALID_USERS[0].get("senha")
     )
     with pytest.raises(HTTPException) as exc:
-        pedido_service.remover_item_pedido(5, usuario)
+        pedido_service.remover_item_pedido(4, usuario)
 
     assert exc.value.status_code == 404
-    assert exc.value.detail == "Item pedido não encontrado!"
+    assert exc.value.detail == "Pedido associado ao item pedido não encontrado!"
