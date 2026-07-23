389c86579cac1b2c358069ce26966ea9d562acc4
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-22 22:21:48 -0300

    testando se consigo criar conta

---
+++ tests/src/routes/test_auth.py
@@ -0,0 +1,14 @@
+from httpx2 import Response
+
+
+def test_auth_routes_se_consigo_criar_uma_conta(client):
+    data = {
+        "email": "teste2@teste2.com",
+        "senha": "teste2",
+        "nome": "teste2",
+        "ativo": True,
+    }
+    response: Response = client.post("/auth/criar-conta", json=data)
+
+    assert response.status_code == 201
+    assert response.json()["nome"] == data.get("nome")
