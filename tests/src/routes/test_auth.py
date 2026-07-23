89e30e3a506edc960b31733b884f679ea48e6a5a
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-22 22:29:37 -0300

    testando se nao consigo criar conta existente

--- tests/src/routes/test_auth.py
+++ tests/src/routes/test_auth.py
@@ -12,3 +12,17 @@ def test_auth_routes_se_consigo_criar_uma_conta(client):
 
     assert response.status_code == 201
     assert response.json()["nome"] == data.get("nome")
+
+
+def test_auth_routes_se_nao_consigo_criar_uma_conta_existente(client):
+    data = {
+        "email": "teste2@teste2.com",
+        "senha": "teste2",
+        "nome": "teste2",
+        "ativo": True,
+    }
+    client.post("/auth/criar-conta", json=data)
+    response: Response = client.post("/auth/criar-conta", json=data)
+
+    assert response.status_code == 409
+    assert response.json()["detail"] == "Conta já existe!"
