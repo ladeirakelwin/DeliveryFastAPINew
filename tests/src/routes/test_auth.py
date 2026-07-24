7e5f4ea60eea411ade6d4a3adb67374136ea2248
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-24 20:48:57 -0300

    testando se rota login-form não retorna token com usuário inválido e inativo

--- tests/src/routes/test_auth.py
+++ tests/src/routes/test_auth.py
@@ -1,4 +1,6 @@
 from httpx2 import Response
+from fastapi.exceptions import HTTPException
+import pytest
 
 
 def test_auth_routes_se_consigo_criar_uma_conta(client):
@@ -39,3 +41,25 @@ def test_auth_routes_se_consigo_obter_token(client_user):
     assert response.status_code == 201
     assert response.json().get("refresh_token") is not None
     assert response.json().get("access_token") is not None
+
+
+def test_auth_routes_se_nao_consigo_obter_token_com_usuario_invalido(client_user):
+    data = {
+        "password": "teste1",
+        "username": "teste2",
+    }
+
+    response: Response = client_user.post("/auth/login-form", data=data)
+
+    assert response.status_code == 401
+
+
+def test_auth_routes_se_nao_consigo_obter_token_com_usuario_inativo(client_user):
+    data = {
+        "password": "inativoa",
+        "username": "inativoa",
+    }
+
+    response: Response = client_user.post("/auth/login-form", data=data)
+
+    assert response.status_code == 403
