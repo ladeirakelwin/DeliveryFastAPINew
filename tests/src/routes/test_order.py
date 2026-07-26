328a1365ef7f1ffaa8cd30d3b0e1a8fe739c221a
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-26 17:35:23 -0300

    testando se não consigo criar pedido na rota /pedidos/criar

--- tests/src/routes/test_order.py
+++ tests/src/routes/test_order.py
@@ -8,3 +8,14 @@ def test_order_routes_se_consigo_criar_pedido(client_user):
 
     assert novo_pedido.status_code == 201
     assert int(novo_pedido.json()["id"]) == 1
+
+
+def test_order_routes_se_não_consigo_criar_pedido(client_user):
+    response_access_token = client_user.post(
+        "/auth/login-form", data={"username": "adm", "password": "adm"}
+    )
+    refresh_token = response_access_token.json()["refresh_token"]
+    headers = {"Authorization": f"Bearer {refresh_token}", "Accept": "application/json"}
+    novo_pedido = client_user.post("/pedidos/criar", headers=headers, data={"id": 1})
+
+    assert novo_pedido.status_code == 401
