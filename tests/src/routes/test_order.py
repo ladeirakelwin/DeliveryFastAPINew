894054a661dbc728d5cb7354e60ef9dfed438e34
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-26 21:42:06 -0300

    teste da rota /pedidos/listar

--- tests/src/routes/test_order.py
+++ tests/src/routes/test_order.py
@@ -109,3 +109,21 @@ def test_order_routes_se_consigo_cancelar_pedido(client_order):
 
         assert pedido.status_code == 200
         assert pedido.json()["pedido"]["status"] == "CANCELADO"
+
+
+def test_order_routes_se_consigo_listar_pedidos(client_order):
+    response_access_token = client_order.post(
+        "/auth/login-form", data={"username": "adm", "password": "adm"}
+    )
+
+    access_token = response_access_token.json()["access_token"]
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "accept": "application/json",
+        "Content-Type": "application/json",
+    }
+
+    pedido = client_order.get("/pedidos/listar", headers=headers)
+
+    assert pedido.status_code == 200
+    assert len(pedido.json()["pedidos"]) == 4
