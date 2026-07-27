6a1a7e1badd51d14b8b24ac56b5e9a72a60eb0b6
ladeirakelwin <ladeirakelwin@gmail.com>
2026-07-26 21:45:07 -0300

    teste da rota se não consigo usar rota /pedidos/listar

--- tests/src/routes/test_order.py
+++ tests/src/routes/test_order.py
@@ -127,3 +127,20 @@ def test_order_routes_se_consigo_listar_pedidos(client_order):
 
     assert pedido.status_code == 200
     assert len(pedido.json()["pedidos"]) == 4
+
+
+def test_order_routes_se_nao_consigo_listar_pedidos(client_order):
+    response_access_token = client_order.post(
+        "/auth/login-form", data={"username": "teste", "password": "teste"}
+    )
+
+    access_token = response_access_token.json()["access_token"]
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "accept": "application/json",
+        "Content-Type": "application/json",
+    }
+
+    pedido = client_order.get("/pedidos/listar", headers=headers)
+
+    assert pedido.status_code == 401
