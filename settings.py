178f2a1d97982804cd6157f1ef3288c8d13497f5
ladeirakelwin <ladeirakelwin@gmail.com>
2026-08-02 21:52:16 -0300

    formatando settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ from pydantic_core import PydanticCustomError, ValidationError
 from functools import lru_cache
 from pydantic import field_validator
 
+
 class Settings(BaseSettings):
     app_name: str = "DELIVERYFASTAPINEW"
     DATABASE_URL: str = ""
@@ -11,18 +12,21 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 3
 
-    @field_validator("DATABASE_URL","SECRET_KEY", mode="before")
+    @field_validator("DATABASE_URL", "SECRET_KEY", mode="before")
     @classmethod
     def validar_variaveis_ambiente_str(cls, value: str):
 
         if not value:
             raise PydanticCustomError(
-                "EnvDependenciesError", "Está faltando as variáveis de ambiente: SECRET_KEY e/ou DATABASE_URL"
+                "EnvDependenciesError",
+                "Está faltando as variáveis de ambiente: SECRET_KEY e/ou DATABASE_URL",
             )
 
         return value
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
 
 
 @lru_cache
