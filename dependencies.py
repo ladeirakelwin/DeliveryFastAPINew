218fb2ffacb43772b3670808a235b0c17f27beb5
ladeirakelwin <ladeirakelwin@gmail.com>
2026-08-02 21:49:47 -0300

    alterando dependencies para utilizar esse validador

--- dependencies.py
+++ dependencies.py
@@ -1,13 +1,15 @@
-import os
-from dotenv import load_dotenv
 from fastapi.security import OAuth2PasswordBearer
+from settings import get_settings, Settings
 
-load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "")
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = str(os.getenv("ALGORITHM"))
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
-REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 3))
+settings = {}
+initial_settings: Settings = get_settings()
+settings = initial_settings.model_dump(mode="python")
+
+DATABASE_URL = settings["DATABASE_URL"]
+SECRET_KEY = settings["SECRET_KEY"]
+ALGORITHM = settings["ALGORITHM"]
+ACCESS_TOKEN_EXPIRE_MINUTES = settings["ACCESS_TOKEN_EXPIRE_MINUTES"]
+REFRESH_TOKEN_EXPIRE_DAYS = settings["REFRESH_TOKEN_EXPIRE_DAYS"]
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login-form")
