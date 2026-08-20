0acd9aadc138a538650ac70391f1d788afe9c8a5
ladeirakelwin <ladeirakelwin@gmail.com>
2026-08-19 21:01:20 -0300

    ajustando retorno do heath check

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from src.routes import orders
 from database import DBSession
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 import time
-
 from src.routes import auth
+from fastapi.responses import JSONResponse
+from loguru import logger
 
 app = FastAPI()
 
@@ -16,24 +17,33 @@ def healthcheck(db: DBSession):
     try:
         db.execute(text("Select 1"))
         latency = (time.time() - start_time) * 1000
-        return {"status": "healthy", "latency_ms": round(latency, 2), "error": None}
+        return {
+            "status": "healthy",
+            "latency_ms": round(latency, 2),
+        }
     except SQLAlchemyError as err:
         # Crucial: Rollback prevents the session from getting stuck in a broken transaction state
         db.rollback()
 
         # Extract the underlying database engine error message if available
         error_msg = str(err.__dict__.get("orig", err))
-        return {
-            "status": "unhealthy",
-            "latency_ms": round((time.time() - start_time) * 1000, 2),
-            "error": error_msg,
-        }
+        logger.error(error_msg)
+        return JSONResponse(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            content={
+                "status": "unhealthy",
+                "latency_ms": round((time.time() - start_time) * 1000, 2),
+            },
+        )
     except Exception as err:
-        return {
-            "status": "unhealthy",
-            "latency_ms": round((time.time() - start_time) * 1000, 2),
-            "error": str(err),
-        }
+        logger.error(str(err))
+        return JSONResponse(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            content={
+                "status": "unhealthy",
+                "latency_ms": round((time.time() - start_time) * 1000, 2),
+            },
+        )
 
 
 app.include_router(auth.auth_routes)
